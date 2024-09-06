7c5dfd40ca6ff8412f6338db7311f601a3e87bf5
Fabiojr7 <fabiojrsilveira09@gmail.com>
2024-09-06 15:41:54 -0300

    feat: create service layer.
    Reviewed-by: Fabiojr7 <fabiojrsilveira09@gmail.com>
    Refs #6

---
+++ src/main/java/br/com/fabiojrsm/auth_practice/services/UserService.java
@@ -0,0 +1,21 @@
+package br.com.fabiojrsm.auth_practice.services;
+
+import br.com.fabiojrsm.auth_practice.domain.user.User;
+import br.com.fabiojrsm.auth_practice.repositories.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class UserService {
+
+    private UserRepository userRepository;
+
+    public Optional<User> getUserById(UUID id){
+        return userRepository.findById(id);
+    }
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+}
